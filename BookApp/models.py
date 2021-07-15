fefeda69b3403f0b7a89b7e2075303b1e4fb0eb7
KhuushiGulati <gulati.khushi.1999@gmail.com>
2021-07-15 14:13:30 +0530

    commit done

--- BookApp/models.py
+++ BookApp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 class AddBook(models.Model):
+    MY_CHOICES = (('English', 'English'),
+                  ('Hindi', 'Hindi'),
+                  )
     BookName=models.CharField(max_length=200)
     Author=models.CharField(max_length=200)
     Publisher=models.CharField(max_length=200)
     Publication_year=models.IntegerField()
     genre=models.CharField(max_length=240)
-    language=models.CharField(max_length=290)
+    language=MultiSelectField(choices=MY_CHOICES)
     Book_description=models.TextField()
 
 
